39275997cc771aab89fa62d01197edef405247e3
Odenktools <odenktools86@gmail.com>
2019-05-19 22:53:01 +0700

    Tambahkan route kampus

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['as' => 'api::', 'namespace' => 'Api', 'prefix' => 'v1'], function () {
+    Route::get('kampus', ['as' => 'kampus.index', 'uses' => 'KampusController@getIndex']);
+    //Route::post('kampus', ['as' => 'kampus.insert', 'uses' => 'KampusController@postInsert']);
+    //Route::put('kampus', ['as' => 'kampus.update', 'uses' => 'KampusController@putUpdate']);
+    Route::post('kampus/image', ['as' => 'kampus.addimage', 'uses' => 'KampusController@postImageKampus']);
+    //Route::delete('kampus/{id}', ['as' => 'kampus.update', 'uses' => 'KampusController@deleteHapus'])->where('id', '[a-zA-Z0-9_-]+');
+});
